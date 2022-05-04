9eef29b82ebafc6c434c09664fcfccc22a828237
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-05 01:23:31 +0530

    Add Nav Component

---
+++ Frontend/src/component/common/UIElements/nav.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+const NavBar = () => {
+  return (
+    <div>
+      <h1>Se</h1>
+
+      <ul>
+        <li>Threader</li>
+        <li>Customer</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import NavBar from "./component/common/UIElements/nav";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
