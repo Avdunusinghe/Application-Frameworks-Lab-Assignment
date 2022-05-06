6ecdef7a700b1bd414b7221ac0556efa0bcb8890
Avdunusinghe <avdunusinghe@gmail.com>
2022-05-06 15:11:05 +0530

    Update Opertion Item

---
+++ Frontend/src/component/pages/treder/item-list/item.list.component.jsx
@@ -0,0 +1,61 @@
+import React, { Component, useCallback, useEffect, useState } from "react";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+
+import itemsService from "../../../../services/items.service";
+
+const ItemListComponent = () => {
+  const [items, setItems] = useState([]);
+
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  useEffect(() => {
+    getAllItems();
+  }, [getAllItems]);
+
+  const getAllItems = useCallback(() => {
+    itemsService.getAllItems().then((response) => {
+      setItems(response.data);
+    });
+  }, []);
+
+  const handelItemSave = () => {
+    navigate("./itemDetails" + location.search);
+  };
+
+  return (
+    <div>
+      <button onClick={handelItemSave}> Add New Item</button>
+
+      <table>
+        <thead>
+          <tr>
+            <td>Actions</td>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, key) => (
+            <tr key={key}>
+              <td>
+                <Link
+                  style={{ display: "block", margin: "1rem 0" }}
+                  to={`${item.id}`}
+                  key={item.id}
+                >
+                  Update
+                </Link>
+              </td>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Outlet />
+    </div>
+  );
+};
+
+export default ItemListComponent;
